f408ad2d5e2b4e157a5dddb0bc685177051df6a6
Ptki_uo <rptrkm16@gmail.com>
2024-02-17 21:58:28 +0900

    Update(2024 2/17 21:58)

--- src/main/java/com/ptk671/rubycraft/block/CrushingMachine.java
+++ src/main/java/com/ptk671/rubycraft/block/CrushingMachine.java
@@ -1,20 +1,17 @@
 package com.ptk671.rubycraft.block;
 
-import com.ptk671.rubycraft.item.Ruby;
-import com.ptk671.rubycraft.item.RubyIngot;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
+import com.ptk671.rubycraft.item.*;
+import net.minecraft.block.*;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.item.*;
 import net.minecraft.tag.ItemTags;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
+import net.minecraft.util.*;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.pitan76.mcpitanlib.api.block.CompatibleBlockSettings;
-import net.pitan76.mcpitanlib.api.util.TextUtil;
+import net.pitan76.mcpitanlib.api.tag.TagKey;
+import net.pitan76.mcpitanlib.api.util.*;
 
 import static com.ptk671.rubycraft.BlockItems.RUBY_ORE_ITEM;
 import static com.ptk671.rubycraft.Items.*;
@@ -28,18 +25,15 @@ public class CrushingMachine extends Block {
     }
 
     @Override
-
-
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (world.isClient) {
             return ActionResult.SUCCESS;
         }
 
-
         ItemStack handStack = player.getMainHandStack();
 
 
-        if (handStack.getItem() instanceof Ruby) {
+        if (ItemUtil.isIn(handStack, (TagKey<Item>) TagKey.create(TagKey.Type.ITEM, new Identifier("c:rubies")))) {
             handStack.setCount(handStack.getCount() - 1);
             player.giveItemStack(new ItemStack(RUBY_DUST.get(), 1));
             return ActionResult.SUCCESS;
@@ -64,25 +58,27 @@ public class CrushingMachine extends Block {
             return ActionResult.SUCCESS;
         }
 
-
         if (handStack.getItem() == RUBY_AXE.get()) {
             handStack.setCount(handStack.getCount() - 1);
             player.giveItemStack(new ItemStack(RUBY_DUST.get(), 3));
             player.giveItemStack(new ItemStack(SAW_DUST.get(), STICK_2));
             return ActionResult.SUCCESS;
         }
+
         if (handStack.getItem() == RUBY_PICKAXE.get()) {
             handStack.setCount(handStack.getCount() - 1);
             player.giveItemStack(new ItemStack(RUBY_DUST.get(), 3));
             player.giveItemStack(new ItemStack(SAW_DUST.get(), STICK_2));
             return ActionResult.SUCCESS;
         }
+
         if (handStack.getItem() == RUBY_SHOVEL.get()) {
             handStack.setCount(handStack.getCount() - 1);
             player.giveItemStack(new ItemStack(RUBY_DUST.get(), 1));
             player.giveItemStack(new ItemStack(SAW_DUST.get(), STICK_2));
             return ActionResult.SUCCESS;
         }
+
         if (handStack.getItem() == RUBY_HOE.get()) {
             handStack.setCount(handStack.getCount() - 1);
             player.giveItemStack(new ItemStack(RUBY_DUST.get(), 2));
@@ -101,16 +97,15 @@ public class CrushingMachine extends Block {
             player.giveItemStack(new ItemStack(SAW_DUST.get(), 8));
             return ActionResult.SUCCESS;
         }
+
         if (handStack.getItem() == Items.STICK) {
             handStack.setCount(handStack.getCount() - 1);
             player.giveItemStack(new ItemStack(SAW_DUST.get(), 4));
             return ActionResult.SUCCESS;
         }
 
-
         player.sendMessage(TextUtil.translatable("msg.rubycraft.crushing_machine_error"), false);
         return ActionResult.FAIL;
-
 
     }
 }
