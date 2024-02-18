67e0e3c34a49c231428af9b1272aec13e9a139eb
mymai1208 <62325307+mymai1208@users.noreply.github.com>
2024-02-18 18:03:02 +0900

    fix rendering a ruby storage

--- src/main/java/com/ptk671/rubycraft/block/RubyStorage.java
+++ src/main/java/com/ptk671/rubycraft/block/RubyStorage.java
@@ -1,30 +1,24 @@
 package com.ptk671.rubycraft.block;
 
-import com.ptk671.rubycraft.BlockEntities;
-import com.ptk671.rubycraft.tile.RubyChestBlockEntity;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ChestBlock;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.block.entity.ChestBlockEntity;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.pitan76.mcpitanlib.api.block.CompatibleBlockSettings;
 import net.pitan76.mcpitanlib.api.block.ExtendBlock;
-import net.pitan76.mcpitanlib.api.block.ExtendBlockEntityProvider;
-import net.pitan76.mcpitanlib.api.event.block.TileCreateEvent;
-import net.pitan76.mcpitanlib.api.tile.ExtendBlockEntity;
-
-import static com.ptk671.rubycraft.Blocks.RUBY_STORAGE;
+import net.pitan76.mcpitanlib.api.event.block.CollisionShapeEvent;
 
 
-public class RubyStorage  extends ExtendBlock {
+public class RubyStorage extends ExtendBlock {
 
+    private VoxelShape SHAPE_BOTTOM = VoxelShapes.cuboid(0.0, 0.1, 0.0, 16.0, 0.2, 16.0);
+    private VoxelShape SHAPE_TOP = VoxelShapes.cuboid(0.0, 15.8, 0.0, 16.0, 15.9, 16.0);
+    private VoxelShape SHAPE = VoxelShapes.union(SHAPE_BOTTOM, SHAPE_TOP);
     public RubyStorage(CompatibleBlockSettings settings) {
         super(settings);
     }
 
 
+    @Override
+    public VoxelShape getCollisionShape(CollisionShapeEvent event) {
+        return SHAPE;
+    }
 }
