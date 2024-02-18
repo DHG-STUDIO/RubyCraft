329c16ac08a14d06180970a68adff168a1eea580
mymai1208 <62325307+mymai1208@users.noreply.github.com>
2024-02-18 18:44:25 +0900

    fix shape

--- src/main/java/com/ptk671/rubycraft/block/RubyStorage.java
+++ src/main/java/com/ptk671/rubycraft/block/RubyStorage.java
@@ -9,8 +9,8 @@ import net.pitan76.mcpitanlib.api.event.block.CollisionShapeEvent;
 
 public class RubyStorage extends ExtendBlock {
 
-    private VoxelShape SHAPE_BOTTOM = VoxelShapes.cuboid(0.0, 0.1, 0.0, 16.0, 0.2, 16.0);
-    private VoxelShape SHAPE_TOP = VoxelShapes.cuboid(0.0, 15.8, 0.0, 16.0, 15.9, 16.0);
+    private VoxelShape SHAPE_BOTTOM = VoxelShapes.cuboid(0.0, 0.1, 0.0, 0, 0.2, 0);
+    private VoxelShape SHAPE_TOP = VoxelShapes.cuboid(0.0, 15.8, 0.0, 15.9, 15.9, 15.9);
     private VoxelShape SHAPE = VoxelShapes.union(SHAPE_BOTTOM, SHAPE_TOP);
     public RubyStorage(CompatibleBlockSettings settings) {
         super(settings);
