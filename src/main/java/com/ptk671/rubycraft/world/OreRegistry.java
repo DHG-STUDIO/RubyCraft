0d947b47e3b8f0564486226619d4225381cb1571
mymai1208 <62325307+mymai1208@users.noreply.github.com>
2024-02-19 20:10:49 +0900

    Refactor code

--- src/main/java/com/ptk671/rubycraft/world/OreRegistry.java
+++ src/main/java/com/ptk671/rubycraft/world/OreRegistry.java
@@ -8,12 +8,11 @@ import net.minecraft.world.gen.placementmodifier.PlacementModifier;
 import net.pitan76.mcpitanlib.api.registry.WorldGenRegistry;
 import net.pitan76.mcpitanlib.api.util.FeatureConfigUtil;
 import net.pitan76.mcpitanlib.api.util.PlacedFutureUtil;
-import org.apache.commons.lang3.concurrent.ConcurrentException;
-import org.apache.commons.lang3.concurrent.LazyInitializer;
 
 import java.util.List;
 
-import static com.ptk671.rubycraft.RubyCraft.*;
+import static com.ptk671.rubycraft.RubyCraft.id;
+import static com.ptk671.rubycraft.RubyCraft.worldGenRegistry;
 
 public class OreRegistry {
     private static final List<PlacementModifier> RUBY_ORE_PLACEMENT_MODIFIERS = PlacedFutureUtil.createPlacementModifiers(
@@ -21,37 +20,18 @@ public class OreRegistry {
             100
     );
 
-    private static final LazyInitializer<ConfiguredFeature<?, ?>> LAZY_RUBY_ORE_CONFIGURE = new LazyInitializer<>() {
-        @Override
-        protected ConfiguredFeature<?, ?> initialize() {
-            return FeatureConfigUtil.createConfiguredFeature(
-                    FeatureConfigUtil.createStoneOreFeatureConfig(
-                            Blocks.RUBY_ORE.get().getDefaultState(),
-                            9
-                    )
-            );
-        }
-    };
-
-    private static final LazyInitializer<PlacedFeature> LAZY_RUBY_ORE_PLACER = new LazyInitializer<>() {
-        @Override
-        protected PlacedFeature initialize() throws ConcurrentException {
-            return PlacedFutureUtil.create(LAZY_RUBY_ORE_CONFIGURE.get(), RUBY_ORE_PLACEMENT_MODIFIERS);
-        }
-    };
-
     public static void register() {
-        try {
-            ConfiguredFeature<?, ?> rubyOreConfigured = LAZY_RUBY_ORE_CONFIGURE.get();
-            PlacedFeature rubyOrePlacer = LAZY_RUBY_ORE_PLACER.get();
+        ConfiguredFeature<?, ?> rubyOreConfigured = FeatureConfigUtil.createConfiguredFeature(
+                FeatureConfigUtil.createStoneOreFeatureConfig(
+                        Blocks.RUBY_ORE.get().getDefaultState(),
+                        9
+                )
+        );
+        PlacedFeature rubyOrePlacer = PlacedFutureUtil.create(rubyOreConfigured, RUBY_ORE_PLACEMENT_MODIFIERS);
 
-            worldGenRegistry.registerFeature(id("ruby_ore_overworld"), () -> rubyOreConfigured);
-            worldGenRegistry.registerPlacedFeature(id("ruby_ore_overworld"), () -> rubyOrePlacer);
+        worldGenRegistry.registerFeature(id("ruby_ore_overworld"), () -> rubyOreConfigured);
+        worldGenRegistry.registerPlacedFeature(id("ruby_ore_overworld"), () -> rubyOrePlacer);
 
-            WorldGenRegistry.replaceProperties(GenerationStep.Feature.UNDERGROUND_ORES, rubyOrePlacer);
-        }
-        catch (ConcurrentException e) {
-            LOGGER.error("Failed to register ruby ore", e);
-        }
+        WorldGenRegistry.replaceProperties(GenerationStep.Feature.UNDERGROUND_ORES, rubyOrePlacer);
     }
 }
