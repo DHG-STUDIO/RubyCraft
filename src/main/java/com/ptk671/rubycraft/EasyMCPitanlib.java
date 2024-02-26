70af66ff3f5a88aa419697899d35bbbd237cbf80
Ptkiuo <rptrkm16@gmail.com>
2024-02-26 17:31:11 +0900

    Update EasyMCPitanlib

--- src/main/java/com/ptk671/rubycraft/EasyMCPitanlib.java
+++ src/main/java/com/ptk671/rubycraft/EasyMCPitanlib.java
@@ -1,23 +1,64 @@
 package com.ptk671.rubycraft;
 
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.block.Block;
-import net.minecraft.client.render.RenderLayer;
 import net.minecraft.item.Item;
-
+import net.minecraft.util.Identifier;
 import net.pitan76.mcpitanlib.api.block.CompatibleBlockSettings;
 import net.pitan76.mcpitanlib.api.block.ExtendBlock;
-import net.pitan76.mcpitanlib.api.item.CompatibleItemSettings;
-import net.pitan76.mcpitanlib.api.item.CreativeTabBuilder;
-import net.pitan76.mcpitanlib.api.item.ExtendItem;
+import net.pitan76.mcpitanlib.api.item.*;
 import net.pitan76.mcpitanlib.api.registry.CompatRegistry;
 import net.pitan76.mcpitanlib.api.registry.result.RegistryResult;
 import net.pitan76.mcpitanlib.api.util.ItemUtil;
 
+import java.util.function.Supplier;
+
 import static com.ptk671.rubycraft.RubyCraft.MOD_ID;
-import static com.ptk671.rubycraft.RubyCraft.id;
 
 public class EasyMCPitanlib {
+
+    //Easy MCPitanlib 1.0.1
+    public static Identifier id(String patch) {
+        return new Identifier(MOD_ID, patch);
+    }
+    //AddRegistry///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    //AddItem
+    public static RegistryResult<Item> addItem(String itemid, CompatibleItemSettings compatibleItemSettings)
+    {
+        RegistryResult<Item> item =  registry.registerItem(id(itemid), () -> new ExtendItem(compatibleItemSettings));
+        return item;
+    }
+
+    //AddBlock
+    public static RegistryResult<Block> addBlock(String blockid, CompatibleBlockSettings compatibleBlockSettings)
+    {
+        RegistryResult<Block> block = registry.registerBlock(id(blockid), () -> new  ExtendBlock(compatibleBlockSettings));
+        return block;
+    }
+
+    //AddBlockItem
+    public static RegistryResult<Item> addBlockItem(Supplier<Block> blockSupplier, String itemid, CompatibleItemSettings compatibleItemSettings)
+    {
+
+        RegistryResult<Item> blockItem =  registry.registerItem(id(itemid), () -> ItemUtil.ofBlock(blockSupplier.get(), compatibleItemSettings));
+        return blockItem;
+    }
+
+    //AddCreativeTAB
+    public static CreativeTabBuilder addCreativeTab(String tabid)
+    {
+        CreativeTabBuilder creativeTab = CreativeTabBuilder.create(id(tabid));
+        registry.registerItemGroup(creativeTab.getIdentifier(), creativeTab);
+        return creativeTab;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+    //EasyRegistry//////////////////////////////////////////////////////////////////////////////////////////////////////
+
     public static CompatRegistry registry = CompatRegistry.createRegistry(MOD_ID);
     //Easy Item
     public static RegistryResult<Item> easyItem(String itemid, CreativeTabBuilder tab)
@@ -25,31 +66,14 @@ public class EasyMCPitanlib {
         RegistryResult<Item> item =  registry.registerItem(id(itemid), () -> new ExtendItem(new CompatibleItemSettings().addGroup(tab)));
         return item;
     }
-    //Easy Block
-    public static RegistryResult<Block> easyBlock(String blockid,CompatibleBlockSettings compatibleBlockSettings)
-    {
-        RegistryResult<Block> block =   registry.registerBlock(id(blockid), () -> new ExtendBlock(compatibleBlockSettings));
-        return block;
-    }
 
-    //Easy BlockItem
-    public static RegistryResult<Item> easyBlockItem(Block block,String itemid,CreativeTabBuilder tab)
+    //EasyBlockItem
+    public static RegistryResult<Item> easyBlockItem(Supplier<Block> blockSupplier, String itemid, CreativeTabBuilder tab)
     {
-        RegistryResult<Item> blockItem =  registry.registerItem(id(itemid), () -> ItemUtil.ofBlock(block, new CompatibleItemSettings().addGroup(tab)));
+
+        RegistryResult<Item> blockItem =  registry.registerItem(id(itemid), () -> ItemUtil.ofBlock(blockSupplier.get(), new CompatibleItemSettings().addGroup(tab)));
         return blockItem;
     }
 
-    //Easy Creative tab
-    public static CreativeTabBuilder easyCreativeTab(String tabid)
-    {
-CreativeTabBuilder creativeTab = CreativeTabBuilder.create(id(tabid));
-        registry.registerItemGroup(creativeTab.getIdentifier(), creativeTab);
-        return creativeTab;
-    }
-  //  TODO: MCPitanLibでBlockRenderLayerMapが作成されたら変更する
-    //Easy Block Render Layers
-    public static void easyBlockRenderLayers(Block block)
-    {
-        BlockRenderLayerMap.INSTANCE.putBlock(block, RenderLayer.getCutout());
-    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
--- src/main/java/com/ptk671/rubycraft/client/BlockRenderLayers.java
+++ src/main/java/com/ptk671/rubycraft/client/BlockRenderLayers.java
@@ -1,12 +1,14 @@
 package com.ptk671.rubycraft.client;
 
 import com.ptk671.rubycraft.Blocks;
-import com.ptk671.rubycraft.EasyMCPitanlib;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class BlockRenderLayers {
     public static void register() {
-        EasyMCPitanlib.easyBlockRenderLayers(Blocks.RUBY_STORAGE.getOrNull());
-        EasyMCPitanlib.easyBlockRenderLayers(Blocks.RUBY_STORAGE_MK2.getOrNull());
-        EasyMCPitanlib.easyBlockRenderLayers(Blocks.RUBY_STORAGE_MK3.getOrNull());
+        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.RUBY_STORAGE.getOrNull(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.RUBY_STORAGE_MK2.getOrNull(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.RUBY_STORAGE_MK3.getOrNull(), RenderLayer.getCutout());
+
     }
 }
